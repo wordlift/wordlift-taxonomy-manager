b4c42e0f58355a0c8109e3220e0b210aa14ae722
Stanimir <stanimir.k.stoyanov@gmail.com>
2018-01-15 13:28:59 +0200

    Load unit test

---
+++ tests/test-wordlift-taxonomy-manager.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Tests: Taxonomy Manager Term Service.
+ *
+ * @since      1.0.0
+ * @package    Wordlift_Taxonomy_Manager
+ * @subpackage Wordlift_Taxonomy_Manager/tests
+ */
+
+/**
+ * Define the {@link Wordlift_Taxonomy_Manager_Term_Service} class.
+ *
+ * @since      1.0.0
+ * @package    Wordlift_Taxonomy_Manager
+ * @subpackage Wordlift_Taxonomy_Manager/tests
+ */
+class Wordlift_Taxonomy_Manager_Term_Service_Test extends WP_UnitTestCase {
+
+
+
+}
